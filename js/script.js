55fad3be862892eb898a78f6d4cca437866959c6
Nikolay <osminin.dev@gmail.com>
2021-02-16 20:16:46 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const answers = [];
+
+// answers[0] = prompt("Ваше имя");
+// answers[1] = prompt("Сколько вам лет?");
+// answers[2] = prompt("Кем вы работаете?");
+
+let obj = document.getElementById("1");
+function func() {
+  document.createElement("<div>");
+}
+
+func();
